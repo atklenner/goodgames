1a6008b5ab5fd0c2b12935142cefced517e82051
Andrew Klenner <atklenner@gmail.com>
2022-09-14 16:21:20 -0700

    connect db

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_STRING, () => {
+  console.log("connected to DB");
+});
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -10,7 +15,7 @@ app.use(express.json());
 app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  res.render("index");
 });
 
 app.listen(process.env.PORT, () => console.log("server is running"));
