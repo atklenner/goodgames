cbe9840330e3166f054ce871223c98925d9e7b37
Andrew Klenner <atklenner@gmail.com>
2022-09-23 15:56:19 -0700

    create game model

---
+++ models/Game.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema({
+  name: { type: String, required: true },
+  genre: { type: String, required: true },
+  rating: {
+    type: String,
+    default: "Unrated",
+    enum: ["Unrated", "Did Not Like", "It Was OK", "Liked It", "Loved It"],
+  },
+  completed: {
+    type: String,
+    default: "No",
+    enum: ["No", "Playing", "Yes", "Quit"],
+  },
+});
+
+module.exports = mongoose.model("Game", gameSchema);
