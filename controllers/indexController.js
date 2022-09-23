6575bdf91684e1938f81929382be2164948251fc
Andrew Klenner <atklenner@gmail.com>
2022-09-23 16:00:25 -0700

    add mongoose to index controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,9 @@
+const Game = require("../models/Game");
+
 module.exports = {
   getHomePage: async (req, res) => {
     try {
-      let games = await gamesCollection.find().toArray();
+      let games = await Game.find().lean();
       res.render("index", { games });
     } catch (error) {
       console.error(error);
