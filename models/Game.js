63599290ad8c30f939d1072223cb56cad7562881
Andrew Klenner <atklenner@gmail.com>
2022-09-26 11:50:23 -0700

    change game model

--- models/Game.js
+++ models/Game.js
@@ -4,16 +4,6 @@ const Schema = mongoose.Schema;
 const gameSchema = new Schema({
   name: { type: String, required: true },
   genre: { type: String, required: true },
-  rating: {
-    type: String,
-    default: "Unrated",
-    enum: ["Unrated", "Did Not Like", "It Was OK", "Liked It", "Loved It"],
-  },
-  completed: {
-    type: String,
-    default: "No",
-    enum: ["No", "Playing", "Yes", "Quit"],
-  },
 });
 
 module.exports = mongoose.model("Game", gameSchema);
