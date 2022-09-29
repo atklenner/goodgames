226817a266e4153d8113ec10ddd5f6355c35a885
Andrew Klenner <atklenner@gmail.com>
2022-09-28 21:06:19 -0700

    write auth routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const indexController = require("../controllers/indexController");
+const authController = require("../controllers/authController");
 
 router.get("/", indexController.getHomePage);
+router.get("/login", authController.getLogin);
+router.post("/login", authController.postLogin);
+router.get("/logout", authController.logout);
+router.get("/signup", authController.getSignup);
+router.post("/signup", authController.postSignup);
 
 module.exports = router;
