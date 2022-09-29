0c678ed89cbb66953308bc9778044e086a6eb6fe
Andrew Klenner <atklenner@gmail.com>
2022-09-28 21:09:17 -0700

    add list model

---
+++ models/List.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listSchema = new Schema({
+  name: { type: String, required: true },
+  userId: { type: mongoose.ObjectId, required: true },
+  listItems: [{ type: mongoose.ObjectId }],
+});
