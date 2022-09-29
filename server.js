359151b38bb1f7aec7afb9b532cc84540a657eba
Andrew Klenner <atklenner@gmail.com>
2022-09-28 20:58:44 -0700

    set up auth boilerplate

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-const database = require("./config/database");
+const connectDB = require("./config/database");
 const indexRouter = require("./routes/index");
 const gamesRouter = require("./routes/games");
+const passport = require("passport");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo");
 
-database();
+require("./config/passport")(passport);
+
+connectDB();
 
 app.set("view engine", "ejs");
 app.use(cors());
@@ -15,6 +21,17 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(
+  session({
+    secret: "my very good secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/games", gamesRouter);
