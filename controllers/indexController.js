ba982d267a59d578f006e15770c61760c4468ccb
Andrew Klenner <atklenner@gmail.com>
2022-10-19 23:34:00 -0700

    add stuff for development

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,22 @@
+const List = require("../models/List");
 const Game = require("../models/Game");
+const ListItem = require("../models/ListItem");
+const User = require("../models/User");
 
 module.exports = {
   getHomePage: async (req, res) => {
     try {
-      res.render("index");
+      let userLists = await List.find({ userID: req.user._id });
+      let listItems = await ListItem.find();
+      let games = await Game.find();
+      let users = await User.find();
+      res.render("index", {
+        user: req.user,
+        lists: userLists,
+        listItems,
+        games,
+        users,
+      });
     } catch (error) {
       console.error(error);
     }
