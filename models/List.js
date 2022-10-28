3eeeb9ed139020e788f03b3784b28759c888f419
Andrew Klenner <atklenner@gmail.com>
2022-10-27 21:27:46 -0700

    add list description

--- models/List.js
+++ models/List.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const listSchema = new Schema({
   name: { type: String, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  description: { type: String },
 });
 
 module.exports = new mongoose.model("List", listSchema);
