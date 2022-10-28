4228de1b9166f57d6f6cd9ea65a9e91674d51d8e
Andrew Klenner <atklenner@gmail.com>
2022-10-27 21:27:39 -0700

    remove review stuff

--- models/ListItem.js
+++ models/ListItem.js
@@ -4,16 +4,6 @@ const Schema = mongoose.Schema;
 const listItemSchema = new Schema({
   gameId: { type: mongoose.Schema.Types.ObjectId, ref: "Game" },
   listId: { type: mongoose.Schema.Types.ObjectId, ref: "List" },
-  rating: {
-    type: String,
-    default: "Unrated",
-    enum: ["Unrated", "Did Not Like", "It Was OK", "Liked It", "Loved It"],
-  },
-  completed: {
-    type: String,
-    default: "No",
-    enum: ["No", "Playing", "Yes", "Quit"],
-  },
   dateAdded: { type: Date, default: Date.now },
 });
 
