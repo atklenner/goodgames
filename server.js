d41c83aafd68d54ab3f37e9ad3847b1f28269929
Andrew Klenner <atklenner@gmail.com>
2022-10-31 20:31:53 -0700

    add lines for readability

--- server.js
+++ server.js
@@ -20,12 +20,18 @@ require("./config/passport")(passport);
 connectDB();
 
 app.set("view engine", "ejs");
+
 app.use(cors());
+
 app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(morgan("tiny"));
+
 app.use(methodOverride("_method"));
+
 app.use(
   session({
     secret: "my very good secret",
