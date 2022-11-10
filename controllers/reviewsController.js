f5c8347957d2fc93b1de884abcf90a1a84fac55f
Andrew Klenner <atklenner@gmail.com>
2022-11-10 11:57:26 -0800

    finish get review routes

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -4,7 +4,14 @@ module.exports = {
   addReviewPage: (req, res) => {
     res.render("./reviews/reviewForm", { gameId: req.params.gameId });
   },
-  getReview: () => {},
+  getReview: async (req, res) => {
+    try {
+      const review = await Review.findOne({ _id: req.params.id });
+      res.render("./reviews/reviewPage", { review });
+    } catch (error) {
+      console.log(error);
+    }
+  },
   addReview: async (req, res) => {
     try {
       let newReview = await Review.create({
