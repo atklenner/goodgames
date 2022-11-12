062ff9eab0790abfb5c7b2d111b5d7ec38a449ea
Andrew Klenner <atklenner@gmail.com>
2022-11-12 12:05:46 -0800

    finish game update router

--- routes/games.js
+++ routes/games.js
@@ -11,7 +11,13 @@ router.get("/:id", gamesController.getOneGame);
 
 router.post("/new-game", upload.single("image"), gamesController.addNewGame);
 
-router.put("/update-game/:id", gamesController.updateGame);
+router.get("/edit-game/:id", gamesController.updateGamePage);
+
+router.put(
+  "/update-game/:id",
+  upload.single("image"),
+  gamesController.updateGame
+);
 
 router.delete("/delete-game/:id", gamesController.deleteGame);
 
