a5dbe66ff40bb5855966ea8d1562c1e7ba08f14c
Andrew Klenner <atklenner@gmail.com>
2022-11-14 11:50:37 -0800

    finish create list controller

--- controllers/listsController.js
+++ controllers/listsController.js
@@ -1,11 +1,11 @@
 const List = require("../models/List");
-// const ListItem = require("../models/ListItem")
+const User = require("../models/User");
 
 module.exports = {
   getAllLists: async (req, res) => {
     try {
       let lists = await List.find().lean();
-      res.render("allLists", { lists, title: "All Lists" });
+      res.render("lists/allLists", { lists, title: "All Lists" });
     } catch (error) {
       console.error(error);
     }
@@ -14,23 +14,42 @@ module.exports = {
     let id = req.params.id;
     try {
       let list = await List.findById(id).lean();
-      res.render("listPage", { list });
+      res.render("lists/listPage", { list });
     } catch (error) {
       console.error(error);
     }
   },
   getLoggedInUserLists: async (req, res) => {
     try {
-      let lists = await List.find().lean();
-      res.render("allLists", { lists, title: "My Lists" });
+      let user = await User.findById(req.user._id).lean();
+      res.render("lists/allLists", { lists: user.lists, title: "My Lists" });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  getListForm: async (req, res) => {
+    try {
+      if (req.params.id) {
+        let list = await List.findById(req.params._id).lean();
+        res.render("lists/listForm", { ...list });
+      } else {
+        res.render("lists/listForm");
+      }
     } catch (error) {
       console.error(error);
     }
   },
   addNewList: async (req, res) => {
     try {
-      let addedList = await List.create(req.body);
-      res.json(addedList);
+      let addedList = await List.create({
+        name: req.body.name,
+        description: req.body.description,
+        userId: req.user._id,
+      });
+      let user = await User.findById(req.user._id);
+      user.lists.push({ name: addedList.name, _id: addedList._id });
+      await user.save();
+      res.redirect("/lists/my-lists");
     } catch (error) {
       console.error(error);
     }
