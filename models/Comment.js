19219125f4e5050df445580049ca09eb58957d2b
Andrew Klenner <atklenner@gmail.com>
2022-11-29 10:58:34 -0800

    forgot some closing braces

--- models/Comment.js
+++ models/Comment.js
@@ -5,9 +5,9 @@ const commentSchema = new Schema({
   user: {
     _id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     username: { type: String },
-  }
+  },
   likes: { type: Number, default: 0 },
   body: { type: String },
-}
+})
 
 module.exports = new mongoose.model("Comment", commentSchema);
