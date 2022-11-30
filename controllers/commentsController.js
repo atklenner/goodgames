454f77b5f08f851bab4d59e6878ebc98de8a84d5
Andrew Klenner <atklenner@gmail.com>
2022-11-30 10:01:16 -0800

    change it to tell client to reload page

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -6,7 +6,7 @@ module.exports = {
       let comment = await Comment.findById(req.params.id);
       if (comment.user._id.toString() === req.user._id.toString()) next();
       await comment.updateOne({ $inc: { likes: 1 } });
-      res.send("all good!");
+      res.redirect("back");
     } catch (error) {
       console.log(error);
     }
@@ -16,7 +16,7 @@ module.exports = {
       let comment = await Comment.findById(req.params.id);
       if (comment.user._id.toString() !== req.user._id.toString()) next();
       await comment.remove();
-      res.send("comment was deleted");
+      res.redirect("back");
     } catch (error) {
       console.log(error);
     }
