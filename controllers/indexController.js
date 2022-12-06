7d619d0527cfd7cb883f5f6e3eb10d583b1f7118
Andrew Klenner <atklenner@gmail.com>
2022-12-06 12:22:24 -0800

    add recent games to user dashboard

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,6 @@
 const List = require("../models/List");
 const User = require("../models/User");
+const Game = require("../models/Game");
 
 module.exports = {
   getHomePage: async (req, res) => {
@@ -8,10 +9,12 @@ module.exports = {
       let mainList = await List.findById(user.mainList)
         .populate("games")
         .lean();
+      let recentGames = await Game.find({}).limit(5).sort({ _id: "desc" }).lean();
       res.render("index", {
         user: req.user,
         lists: user.lists,
         mainList,
+        recentGames,
       });
     } catch (error) {
       console.error(error);
