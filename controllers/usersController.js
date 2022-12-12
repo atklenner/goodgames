785a8ee3e2045341cdf0d1c10037459da78e6367
Andrew Klenner <atklenner@gmail.com>
2022-12-12 12:18:07 -0800

    add more user info to the user page

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const Review = require("../models/Review");
 
 module.exports = {
   getAllUsers: async (req, res, next) => {
@@ -12,8 +13,13 @@ module.exports = {
   },
   getOneUser: async (req, res, next) => {
     try {
-      let user = await User.findById(req.params.id);
-      res.render("users/userProfile", { user });
+      let user = await User.findById(req.params.id).populate("lists").lean();
+      let lists = user.lists;
+      if (user._id.toString !== req.user._id.toString()) {
+        lists = lists.filter((list) => !list.private);
+      }
+      let reviews = await Review.find({ "user._id": user._id }).lean();
+      res.render("users/userProfile", { user, reviews, lists: user.lists });
     } catch (error) {
       console.error(error);
       next(error);
